f226bf2f75fe1d810f95093b7fcb7564f832be2f
Firna <vyrnvyrd@gmail.com>
2024-08-06 21:55:42 +0700

    refactor(bottom): change width and add cursor pointer

--- src/Components/Bottom.js
+++ src/Components/Bottom.js
@@ -1,3 +1,6 @@
+import { AiFillInstagram } from 'react-icons/ai';
+import { IoLogoFacebook } from 'react-icons/io';
+
 const Bottom = () => {
   return (
     <div className="bg-[#222224] text-white px-8 py-6 lg:px-[80px] lg:py-[60px]">
@@ -9,33 +12,37 @@ const Bottom = () => {
             giving them the courage and confidence to power through each day
             with their choice of elegant and charming contemporary jewellery.
           </p>
-          <div className="bg-bottom-one h-[32px] md:w-[30%] lg:w-[50%] mt-5"></div>
+          <div className="bg-bottom-one h-[32px] md:w-[30%] mt-5"></div>
         </div>
         <div className="text-[14px]">
           <p className="font-bold">COLLECTIONS</p>
-          <p className="mt-4">Rings</p>
-          <p className="mt-4">Earrings</p>
-          <p className="mt-4">Necklaces</p>
-          <p className="mt-4">Bracelets</p>
-          <p className="mt-4">Pendants</p>
+          <p className="mt-4 cursor-pointer">Rings</p>
+          <p className="mt-4 cursor-pointer">Earrings</p>
+          <p className="mt-4 cursor-pointer">Necklaces</p>
+          <p className="mt-4 cursor-pointer">Bracelets</p>
+          <p className="mt-4 cursor-pointer">Pendants</p>
         </div>
         <div className="text-[14px]">
           <p className="font-bold">INFORMATION</p>
-          <p className="mt-4">FAQ</p>
-          <p className="mt-4">Help & Support</p>
-          <p className="mt-4">Terms of Service</p>
-          <p className="mt-4">Privacy Policy</p>
+          <p className="mt-4 cursor-pointer">FAQ</p>
+          <p className="mt-4 cursor-pointer">Help & Support</p>
+          <p className="mt-4 cursor-pointer">Terms of Service</p>
+          <p className="mt-4 cursor-pointer">Privacy Policy</p>
         </div>
         <div className="hidden lg:inline text-[14px]">
           <p className="font-bold">KNOW MORE</p>
-          <p className="mt-4">All about clarities</p>
-          <p className="mt-4">Partnerships</p>
+          <p className="mt-4 cursor-pointer">All about clarities</p>
+          <p className="mt-4 cursor-pointer">Partnerships</p>
         </div>
         <div className="text-[14px] mt-5 lg:mt-0 col-span-2 md:col-span-1 flex justify-end md:inline">
           <div>
             <p className="font-bold">CONTACT INFO</p>
-            <p className="mt-4">(+603) 95441361</p>
-            <p className="mt-4">info@lediamonds.com</p>
+            <p className="mt-4 cursor-pointer">(+603) 95441361</p>
+            <p className="mt-4 cursor-pointer">info@lediamonds.com</p>
+            <div className="hidden lg:flex mt-4">
+              <IoLogoFacebook className="text-[18px] ml-5 mr-1 cursor-pointer" />
+              <AiFillInstagram className="text-[18px] ml-1 cursor-pointer" />
+            </div>
           </div>
         </div>
       </div>
