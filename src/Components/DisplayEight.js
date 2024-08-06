8a155559722ad85e1cd27d7ea59dc76c7b01c5a5
Firna <vyrnvyrd@gmail.com>
2024-08-06 21:12:51 +0700

    refactor(display-eight): bold text

--- src/Components/DisplayEight.js
+++ src/Components/DisplayEight.js
@@ -4,7 +4,7 @@ const DisplayEight = () => {
   return (
     <div className="flex flex-wrap justify-center items-center h-[110px] lg:h-[100px] bg-[#9B622F] text-white">
       <div className="flex items-center mt-3 md:mt-0">
-        <p className="text-[20px] lg:text-[28px] font-cabin pr-2 md:pr-[40px] lg:pr-[60px] border-r-2">
+        <p className="font-bold text-[20px] lg:text-[28px] font-cabin pr-2 md:pr-[40px] lg:pr-[60px] border-r-2">
           FREE DELIVERY
         </p>
         <p className="px-2 md:px-[40px] lg:px-[60px] text-[14px] font-cabin">
